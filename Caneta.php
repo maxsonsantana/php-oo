bdcd1a10937206e702339f77364adc94e825b322
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-10-21 00:04:55 -0300

    Construtores

--- Caneta.php
+++ Caneta.php
@@ -9,6 +9,28 @@ class Caneta {
     protected $carga;
     protected $tampada;
 
+    //Construtor[Também é possível criar o método construtor com o mesmo nome da classe. Ex.: public function Caneta(){}]    
+    public function __construct($m, $c, $p){
+        $this->modelo = $m;
+        $this->cor = $c;
+        $this->ponta = $p;
+        $this->tampar();
+    }
+
+    //Get and Setters
+    public function getModelo(){
+        return $this->modelo;
+    }
+    public function setModelo($m){
+        $this->modelo = $m;
+    }
+    public function getPonta(){
+        return $this->ponta;
+    }
+    public function setPonta($p){
+        $this->ponta = $p;
+    }
+
     public function rabiscar(){
         if($this->tampada == true){
             echo "A caneta está tampada, não dar para rabiscar";
--- index.php
+++ index.php
@@ -4,10 +4,15 @@
         <title>PHP - OO</title>
     </head>
     <body>
+        <pre>
         <?php
             require_once 'Caneta.php';
-            $c1 = new Caneta;
+            $c1 = new Caneta("BIC", "AZUL", 0.7);
+            $c2 = new Caneta("COMPACTOR", "PRETA", 0.5);
             $c1->cor = "Azul";
+           // $c1->modelo = "Compactor";
+            $c1->setModelo("BIC");
+            $c1->setPonta(0.7);
             //$c1->ponta = 0.5;
             //$c1->tampada = true;
             //$c1->destampar();
@@ -20,7 +25,13 @@
 
             print_r($c1);
             echo '<br><br>';
+
+            print "I have a Pencil {$c1->getModelo()} of tip {$c1->getPonta()}";
+            echo '<br><br>';
+            print "Now I have a Pencil {$c2->getModelo()} of tip {$c2->getPonta()}";
+            echo '<br><br>';
         ?>
+        </pre>
     </body>
 
 </html>
